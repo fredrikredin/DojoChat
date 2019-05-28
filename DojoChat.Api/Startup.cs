f0a72d51844569a569507b5333e36f63599dede9
Fredrik Redin <fredrik.redin@raysearchlabs.com>
2019-05-28 09:20:29 +0200

    added swagger frontend

--- DojoChat.Api/Startup.cs
+++ DojoChat.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DojoChat.Api
 {
@@ -26,6 +27,29 @@ namespace DojoChat.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "DojoChat API",
+                    Version = "v1",
+                    Description = "DojoChat is a simple example ASP.NET Core Web API to send/get messages",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Fredrik Redin",
+                        Email = string.Empty,
+                        Url = "https://github.com/fredrikredin"
+                    },
+                    License = new License
+                    {
+                        Name = "No license, free to use",
+                        Url = "https://github.com/fredrikredin/DojoChat"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +63,17 @@ namespace DojoChat.Api
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DojoChat API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
